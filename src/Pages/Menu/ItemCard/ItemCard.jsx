5f9fc38008f0b52a43e5c53abdd0f29120076705
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-02-01 23:25:09 +0300

    First d

---
+++ src/Pages/Menu/ItemCard/ItemCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './item-cart.scss'
+
+const ItemCard = () => {
+    return (
+        <div item-card>
+            <h1>CardItem</h1>
+        </div>
+    );
+};
+
+export default ItemCard;

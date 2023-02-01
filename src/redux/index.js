c719e56f551602ba3a5ad21c43756164c7c7800a
Daniil Grebnev <97984138+daniilgrebnev@users.noreply.github.com>
2023-02-01 13:40:54 +0300

    Delete src directory

--- src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import CartReducer from './cart/reducer'
-
-export const store = configureStore({
-    reducer: {
-        cart: CartReducer,
-    },
-})
